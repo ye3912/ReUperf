604301d088b293992d0eae13be454bfb2ea9263d
ye3912 <2966443912@qq.com>
2026-04-10 00:31:03 +0800

    feat: local build success, ready for CI

---
+++ build/CMakeFiles/thread_scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_scheduler.dir/link.d"
+  "CMakeFiles/thread_scheduler.dir/main.cpp.o"
+  "CMakeFiles/thread_scheduler.dir/main.cpp.o.d"
+  "thread_scheduler"
+  "thread_scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread_scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
